309beef50f8653adc7487f956b4b74870ddcd7fa
roblyn <anrobmari@gmail.com>
2019-11-04 20:52:15 +0100

    first commit

---
+++ connect.php
@@ -0,0 +1,19 @@
+<?php 
+ $server ='localhost';
+ $username='root';
+ $password='';
+ $db='php';
+
+ $conn = new mysqli($server,$username,$password);
+
+ if($conn->connect_error){
+
+     die("Connection failed" . $conn->connect_error);
+
+ }else {
+        echo 'Connected';
+ }
+
+
+
+?>
---
+++ send.php
@@ -0,0 +1,23 @@
+<?php
+    
+    validate_login($_POST['username'], $_POST['psw']);
+
+  
+    function validate_login($username,$password){
+    include 'connect.php';
+    
+    $sql = "SELECT * FROM php.users  WHERE user='".$username."' AND password='".$password."'  ";
+    $result = $conn->query($sql)or die ("Failed to validate user " .mysqli_error($conn) );
+    $count = mysqli_num_rows($result) ;
+    if($count > 0){
+        echo '<script>';
+        echo 'alert("Welcome!!");';
+    echo '</script>';
+    }else{
+        echo '<script>';
+				echo 'alert("Wrong password or Username");';
+				echo 'window.location.href="index.php";';
+			echo '</script>';
+    }
+}
+    ?>
