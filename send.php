9dc14e0f9879211f37755cfd9a26cc48919d5dfc
roblyn <anrobmari@gmail.com>
2019-11-06 19:14:47 +0100

    Erase echo "Welcome"

--- send.php
+++ send.php
@@ -6,12 +6,12 @@
     function validate_login($username,$password){
     include 'connect.php';
     
-    $sql = "SELECT * FROM php.users  WHERE user='".$username."' AND password='".$password."'  ";
+    $sql = "SELECT * FROM users  WHERE user='".$username."' AND password='".$password."'  ";
     $result = $conn->query($sql)or die ("Failed to validate user " .mysqli_error($conn) );
     $count = mysqli_num_rows($result) ;
     if($count > 0){
         echo '<script>';
-        echo 'alert("Welcome!!");';
+      
     echo '</script>';
     }else{
         echo '<script>';
